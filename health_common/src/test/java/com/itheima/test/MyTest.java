42107046d24458cb70c2414f5d6ee0989397daed
zkuzma <zkuzma@163.com>
2020-04-15 18:15:05 +0800

    提交健康项目测试

---
+++ health_common/src/test/java/com/itheima/test/MyTest.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+/**
+ * @auther 大雄
+ * @create 2020-04-15 18:12
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("大雄的测试!");
+    }
+}
